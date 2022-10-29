a89e3cd09b263ac5ca09b075b40d9dca03e72a9c
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2022-10-30 02:09:06 +0530

    Update settings.py

--- pusherapp/pusherapp/settings.py
+++ pusherapp/pusherapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-o32+52!)6710cz6#fp=+m-f(g_!8bgljsto0rcerp&#1kl+g)9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'push-message.herokuapp.com']
 
 
 # Application definition
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
